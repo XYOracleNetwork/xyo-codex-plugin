35fe5ab955dc892667b768338aa6fc3d48b5e2c4
Joel Carter <5701833+JoelBCarter@users.noreply.github.com>
2026-06-05 06:39:44 -0500

    chore: remove pre-nesting layout

--- skills/xl1-scaffold/scripts/scaffold/templates/node/src/index.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config'
-
-import { MainNetwork } from '@xyo-network/xl1-sdk'
-
-console.log('Hello world')
-console.log(`XL1 network id: ${MainNetwork.id}`)
--- skills/xl1-scaffold/scripts/scaffold/templates/react/eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import { recommendedConfig as xylabsConfig } from '@xylabs/eslint-config-react-flat'
-
-export default [
-  { ignores: ['dist/', 'node_modules/', 'coverage/'] },
-  ...xylabsConfig,
-]
